8fdccd53cf9d7180b110330e2b8d6aed1ab0d3b8
ishak96 <ishakayad96@outlook.fr>
2019-07-28 16:24:57 +0200

    add debug option to the code

--- Hachages/Hachage_Coalescent.c
+++ Hachages/Hachage_Coalescent.c
@@ -6,6 +6,7 @@
 #include <time.h>
 
 #define FUNC_ERROR -1
+#define DEBUG 0
 
 typedef enum {VIDE, LIBRE, OCCUPE} TEtat;
 
@@ -33,70 +34,84 @@ typedef struct
 }TableHachage;
 
 TEtat etat(int i, TableHachage th){
-	if( th.tabH == NULL || i < 0 ){
-		fprintf(stderr, "invalid argument ! : etat\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( th.tabH == NULL || i < 0 ){
+			fprintf(stderr, "invalid argument ! : etat\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	return th.tabH[i].etat;
 }
 
 int lien(int i, TableHachage th){
-	if( th.tabH == NULL || i < 0 || etat(i, th) == VIDE ){
-		fprintf(stderr, "invalid argument ! : lien\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( th.tabH == NULL || i < 0 || etat(i, th) == VIDE ){
+			fprintf(stderr, "invalid argument ! : lien\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	return th.tabH[i].lien;
 }
 
 Client client(int i, TableHachage th){
 	Client cli = {0};
-	if( th.tabH == NULL || i < 0 || etat(i, th) == VIDE ){
-		fprintf(stderr, "invalid argument ! : client\n");
-		return cli;
-	}
+	#if(DEBUG > 0)
+		if( th.tabH == NULL || i < 0 || etat(i, th) == VIDE ){
+			fprintf(stderr, "invalid argument i = %d! : client\n", i);
+			return cli;
+		}
+	#endif
 
 	return th.tabH[i].donnee;
 }
 
 int estEtat(int i, TableHachage th, TEtat VEtat){
-	if( th.tabH == NULL || i < 0 ){
-		fprintf(stderr, "invalid argument ! : estEtat\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( th.tabH == NULL || i < 0 ){
+			fprintf(stderr, "invalid argument i = %d ! : estEtat\n", i);
+			return FUNC_ERROR;
+		}
+	#endif
 
 	return ( etat(i, th) == VEtat );
 }
 
 int equals(Client c1, Client c2){
-
-	if( c1.nom == NULL || c2.nom == NULL || c1.prenom == NULL || c2.prenom == NULL ){
-		fprintf(stderr, "invalid argument ! : equals\n");
-		return 0;
-	}
+	#if(DEBUG > 0)
+		if( c1.nom == NULL || c2.nom == NULL || c1.prenom == NULL || c2.prenom == NULL ){
+			fprintf(stderr, "invalid argument ! : equals\n");
+			return 0;
+		}
+	#endif
 
 	return c1.cvc == c2.cvc && !strcmp(c1.nom, c2.nom) && !strcmp(c1.prenom, c2.prenom)
 			&& !strcmp(c1.numero_carte, c2.numero_carte) && strcmp(c1.date, c2.date);
 }
 
 int alloc(TableHachage* th, int P, int R){
-
-	if(P == 0 || R < 0){
-		fprintf(stderr, "invalid argument ! : alloc\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if(P == 0 || R < 0){
+			fprintf(stderr, "invalid argument ! : alloc\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	th->tabH = malloc( ( P + R) * sizeof(Couple) );
 	th->taille = P + R;
 
-	if(th->tabH == NULL){
-		fprintf(stderr, "can't allocate the tabel ! : alloc\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if(th->tabH == NULL){
+			fprintf(stderr, "can't allocate the tabel ! : alloc\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
-	for(int i = 0; i < ( P + R ); i++)
+	for(int i = 0; i < ( P + R ); i++){
 		th->tabH[i].etat = LIBRE;
+		th->tabH[i].lien = 0;
+	}
 
 	return 0;
 }
@@ -128,7 +143,7 @@ int inserHCO(Client elt, TableHachage* th){
 	int lib = 0;
 	int arret = 0;
 
-	while( !arret && l == 0 ){
+	while( !arret && l != 0 ){
 		if( estEtat(i, *th, LIBRE) && lib == 0 ){
 			lib = i;
 		}
@@ -143,16 +158,19 @@ int inserHCO(Client elt, TableHachage* th){
 	}
 
 	if( lib == 0 ){
-		if( arret )
+		if( arret ){
 			return 0;
+		}
 
 		int R = th->taille;
 
-		while( R >= 0 && !estEtat(R, *th, VIDE) )
+		while( R >= 0 && !estEtat(R, *th, VIDE) ){
 			R = R - 1;
+		}
 
-		if( R == 0 )
+		if( R == 0 ){
 			return 1;
+		}
 
 		th->tabH[i].lien = R;
 		th->tabH[R].donnee = elt;
@@ -164,8 +182,9 @@ int inserHCO(Client elt, TableHachage* th){
 		th->tabH[lib].donnee = elt;
 		th->tabH[lib].etat = OCCUPE;
 
-		if( arret )
+		if( arret ){
 			th->tabH[i].etat = LIBRE;
+		}
 
 		return 0;
 	}
@@ -173,10 +192,12 @@ int inserHCO(Client elt, TableHachage* th){
 }
 
 int rechercheHCO(Client elt, TableHachage th){
-	if( th.tabH == NULL ){
-		fprintf(stderr, "invalid argument ! : rechercheHCO\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( th.tabH == NULL ){
+			fprintf(stderr, "invalid argument ! : rechercheHCO\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	int i = hach(elt, th.taille);
 	int arret = 0;
@@ -196,10 +217,12 @@ int rechercheHCO(Client elt, TableHachage th){
 }
 
 int suppHCO(Client elt, TableHachage* th){
-	if( th->tabH == NULL ){
-		fprintf(stderr, "invalid argument ! : suppHCO\n");
-		return 0;
-	}
+	#if(DEBUG > 0)
+		if( th->tabH == NULL ){
+			fprintf(stderr, "invalid argument ! : suppHCO\n");
+			return 0;
+		}
+	#endif
 
 	int i = rechercheHCO(elt, *th);
 
@@ -219,32 +242,41 @@ int creatTable(char* data_file_name, TableHachage* th){
 	char nom[15];
 	char prenom[15];
 
-	if( data_file_name == NULL || th == NULL ){
-		fprintf(stderr, "creatTable: invalid argument!\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( data_file_name == NULL || th == NULL ){
+			fprintf(stderr, "creatTable: invalid argument!\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	data_file=fopen(data_file_name, "r+");
 
-	if( data_file == NULL ){
-		fprintf(stderr, "creatTable: open file!\n");
-		return FUNC_ERROR;
-	}
+	#if(DEBUG > 0)
+		if( data_file == NULL ){
+			fprintf(stderr, "creatTable: open file!\n");
+			return FUNC_ERROR;
+		}
+	#endif
 
 	fscanf(data_file, "number of client = %d\n", &number_client);
 	alloc(th, number_client, rand()%10 + 1);
 
 	for(int i = 0; i < number_client; i++){
 		fscanf(data_file, "%s %d %s %s %s\n", numero_carte, &cvc, date, nom, prenom);
-		if( strlen(numero_carte) > 16 || strlen(date) > 7 || strlen(nom) > 15 || strlen(prenom) > 15 ){
-			fprintf(stderr, "creatTable: ivalid argument on data file line %d!\n", i + 2);
-			return FUNC_ERROR;		
-		}
+		#if(DEBUG > 0)
+			if( strlen(numero_carte) > 16 || strlen(date) > 7 || strlen(nom) > 15 || strlen(prenom) > 15 ){
+				fprintf(stderr, "creatTable: ivalid argument on data file line %d!\n", i + 2);
+				return FUNC_ERROR;		
+			}
+		#endif
 		Client client = {cvc, nom, prenom, numero_carte, date};
 
+		
 		if( inserHCO(client, th) ){
-			fprintf(stderr, "creatTable: overflow!\n");
-			return FUNC_ERROR;				
+			#if(DEBUG > 0)
+				fprintf(stderr, "creatTable: overflow!\n");
+				return FUNC_ERROR;	
+			#endif			
 		}
 	}
 
@@ -263,8 +295,10 @@ int main(int argc, char **argv){
 	}
 
 	if(creatTable(argv[1], &th) < 0){
-		fprintf(stderr, "main: creatTable!\n");
-		return FUNC_ERROR;		
+		#if(DEBUG > 0)
+			fprintf(stderr, "main: creatTable!\n");
+			return FUNC_ERROR;	
+		#endif
 	}
 
 	return 0;
